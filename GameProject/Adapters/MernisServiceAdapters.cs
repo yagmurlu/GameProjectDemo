e7a45c58d0bfc596328aa7a108edf8aeb73d6705
yagmurlu <mavibulut0106@gmail.com>
2021-06-27 00:08:39 +0300

    Concrete

--- GameProject/Adapters/MernisServiceAdapters.cs
+++ GameProject/Adapters/MernisServiceAdapters.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace GameProjectDemo.Adapters
 {
-    public class MernisServiceAdapters : ICheckService
+    public class MernisServiceAdapters
     {
         public bool CheckIfRealPerson(Gamer gamer)
         {
---
+++ GameProject/Concrete/GamerManager.cs
@@ -0,0 +1,41 @@
+﻿using GameProjectDemo.Abstract;
+using GameProjectDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject.Concrete
+{
+    public class GamerManager : IGamerService
+    {
+        public GamerManager()
+        {
+
+        }
+        private ICheckService _checkService;
+        public GamerManager(ICheckService checkService)
+        {
+            _checkService = checkService;
+        }
+        public void Add(Gamer gamer)
+        {
+            if (true)
+            {
+
+            }
+        }
+
+
+        public void Remove(Gamer gamer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Gamer gamer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
